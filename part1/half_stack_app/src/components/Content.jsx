dba2e643dd2a4a52e339e944e9db389bc17d5a1e
DanTenorio <juantenorio16@augustana.edu>
2023-03-27 13:58:13 -0500

    1.3 complete

--- part1/half_stack_app/src/components/Content.jsx
+++ part1/half_stack_app/src/components/Content.jsx
@@ -1,17 +1,12 @@
 import Part from "./Part"
 
-const Content = () => {
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+const Content = (props) => {
+
     return (
         <>
-            <Part part={part1} exercises={exercises1} />
-            <Part part={part2} exercises={exercises2} />
-            <Part part={part3} exercises={exercises3} />
+            <Part part={props.part1} />
+            <Part part={props.part2} />
+            <Part part={props.part3} />
         </>
     )
 }
