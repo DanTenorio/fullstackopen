cc46ededfa1c0973ddba7bcfcf1c9ec8a31e1f42
DanTenorio <juantenorio16@augustana.edu>
2023-04-06 14:27:22 -0500

    part2.2 complete

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,9 +1,11 @@
 import Part from "./Part"
 
 const Content = ({ parts }) => {
+    const totalExercises = parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)
     return (
         <>
             {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
+            <p>total of {totalExercises} exercises</p>
         </>
     )
 }
