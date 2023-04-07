5341231c64c86c81e63dc9ed499e4d3b02e61625
DanTenorio <juantenorio16@augustana.edu>
2023-04-06 20:16:43 -0500

    2.8 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,25 +3,31 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '000-000-0000' }
   ])
   const [newName, setNewName] = useState('')
+  const [newPhoneNumb, setNewPhoneNum] = useState('000-000-0000')
 
   const handleNameChange = (event) => {
-    event.preventDefault()
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    setNewPhoneNum(e.target.value)
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
     const newNameObject = {
-      name: newName
+      name: newName,
+      number: newPhoneNumb
     }
     const repeatedName = persons.find(person => JSON.stringify(person.name) === JSON.stringify(newName))
 
     if (repeatedName === undefined) {
       setPersons(persons.concat(newNameObject))
-      setNewName('  ')
+      setNewName('')
+      setNewPhoneNum('')
     } else {
       alert(`${newName} is already in the phonebook`)
     }
@@ -37,11 +43,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          phone number: <input value={newPhoneNumb} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, index) => <p key={index}>{person.name}</p>)}
+      {persons.map((person, index) => <p key={index}>{person.name} {person.number}</p>)}
     </div>
   )
 }
